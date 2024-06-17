260c86923d86deb0131c2445d51bd374fc6cd5b6
rebot333 <rebot333gaming@gmail.com>
2024-06-16 23:10:52 -0600

    1.20.1 backport

--- src/main/java/carpetlabaddition/LABSettings.java
+++ src/main/java/carpetlabaddition/LABSettings.java
@@ -6,7 +6,4 @@ import static carpet.api.settings.RuleCategory.*;
 
 public class LABSettings {
     public static final String LAB = "LAB";
-
-    @Rule(categories = {LAB, FEATURE})
-    public static boolean hoppersDontStackShulkers = false;
 }
